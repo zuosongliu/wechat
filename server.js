243e9b53fb34ba5763842e017c5bb3dfdea06c25
zuosongliu <38774601+zuosongliu@users.noreply.github.com>
2018-05-03 09:20:21 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var routesEngine = require('./index.js'); 
+var app = express();
+routesEngine(app);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port, process.cwd(): ' + process.cwd() );
+});
+In implementation of index.js, now we need to implement verification process:
+var request = require('request');
+var jsSHA = require('jssha');
+
+module.exports = function (app) {
+  app.route('/').get(function(req,res){
+    var token="sandy"; // replace it with your own token
+    var signature = req.query.signature,
+      timestamp = req.query.timestamp,
+      echostr   = req.query.echostr,
+      nonce     = req.query.nonce;
+      oriArray = new Array();
+      oriArray[0] = nonce;
+      oriArray[1] = timestamp;
+      oriArray[2] = token;
+      oriArray.sort();
+      var original = oriArray.join('');
+
+      var shaObj = new jsSHA("SHA-1", 'TEXT');
+      shaObj.update(original);
+      var scyptoString = shaObj.getHash('HEX');
+      console.log("calculated string: " + scyptoString);
+     if (signature == scyptoString) {
+        res.send(echostr);
+     } else {
+        res.send('bad token');
+     }
+  });
+};
